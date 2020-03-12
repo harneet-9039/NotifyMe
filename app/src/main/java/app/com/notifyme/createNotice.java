b6f3d12b8ca0800c7d62d622a7b452ad6a52ff2d
LizaMNNIT <singlaliza98@gmail.com>
2020-03-13 00:14:24 +0530

    create notice activity

---
+++ app/src/main/java/app/com/notifyme/createNotice.java
@@ -0,0 +1,14 @@
+package app.com.notifyme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class createNotice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_notice);
+    }
+}
