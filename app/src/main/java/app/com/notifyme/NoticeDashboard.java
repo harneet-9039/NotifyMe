7b4f7cae127ecf5a9bd72c75c601ed6543b84afd
Harneet Singh <harneet.singh.ts@gmail.com>
2020-03-12 22:50:28 +0530

    SearchView Layout updated

--- app/src/main/java/app/com/notifyme/NoticeDashboard.java
+++ app/src/main/java/app/com/notifyme/NoticeDashboard.java
@@ -1,10 +1,13 @@
 package app.com.notifyme;
 
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import android.view.View;
+
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -17,6 +20,7 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
+import android.widget.SearchView;
 
 public class NoticeDashboard extends AppCompatActivity {
 
@@ -28,16 +32,9 @@ public class NoticeDashboard extends AppCompatActivity {
         setContentView(R.layout.activity_notice_dashboard);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
+        getSupportActionBar().setTitle("Notify Me");
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
@@ -48,6 +45,33 @@ public class NoticeDashboard extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        SearchView simpleSearchView =  findViewById(R.id.searchView);
+        UpdateSearchView(simpleSearchView);
+
+
+    }
+
+    private void UpdateSearchView(SearchView searchView){
+        searchView.setActivated(true);
+        searchView.setQueryHint("Search Notice..");
+        searchView.onActionViewExpanded();
+        searchView.setIconified(false);
+        searchView.clearFocus();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // do something on text submit
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // do something when text changes
+                return false;
+            }
+        });
     }
 
     @Override
@@ -57,7 +81,7 @@ public class NoticeDashboard extends AppCompatActivity {
         return true;
     }
 
-    @Override
+   @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
