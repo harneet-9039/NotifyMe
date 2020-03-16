7d08b47e30c4444fbc488eeecd6047d3cce9f577
LizaMNNIT <singlaliza98@gmail.com>
2020-03-16 16:54:22 +0530

    departments and courses loaded

--- app/src/main/java/app/com/notifyme/createNotice.java
+++ app/src/main/java/app/com/notifyme/createNotice.java
@@ -4,6 +4,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.Intent;
@@ -19,13 +20,26 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.google.android.material.snackbar.Snackbar;
 
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -35,9 +49,25 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
+import app.com.common.GlobalMethods;
+import app.com.common.Singleton;
+import app.com.models.Course;
+import app.com.models.Department;
+
 public class createNotice extends AppCompatActivity {
+
+    private ProgressDialog progressDialog;
+    private Spinner Course_Spinner, Department_Spinner,Scope_Spinner,Year_Spinner;
+
+    private ArrayList<app.com.models.Department> Department;
+    private ArrayList<app.com.models.Course> Course;
+
+    private static int DepartmentID, CourseID;
 
     static final int REQUEST_IMAGE_CAPTURE=1;
     private static final int PICKFILE_RESULT_CODE = 8778;
@@ -51,6 +81,44 @@ public class createNotice extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_notice);
+
+        Department_Spinner=findViewById(R.id.spinner1);
+        Course_Spinner=findViewById(R.id.spinner2);
+        Scope_Spinner=findViewById(R.id.spinner);
+        Year_Spinner=findViewById(R.id.spinner3);
+        progressDialog = new ProgressDialog(this);
+
+
+
+        Scope_Spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+
+                String selected=Scope_Spinner.getSelectedItem().toString();
+                if(selected.equals("Public"))
+                {
+                    Department_Spinner.setEnabled(false);
+                    Course_Spinner.setEnabled(false);
+                    Year_Spinner.setEnabled(false);
+                }
+
+                else if(selected.equals("Private"))
+                {
+                    Department_Spinner.setEnabled(true);
+                    Course_Spinner.setEnabled(true);
+                    Year_Spinner.setEnabled(true);
+                    Initialize();
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+
+        });
+
 
         v = findViewById(R.id.root);
         ContextWrapper cw = new ContextWrapper(getApplicationContext());
@@ -68,6 +136,143 @@ public class createNotice extends AppCompatActivity {
         }
 
     }
+
+    private void Initialize() {
+
+
+        Department=new ArrayList<>();
+        Course=new ArrayList<>();
+
+        FillDepartment();
+
+        Department_Spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if(i>0)
+                {
+                    final Department department = (Department) Department_Spinner.getItemAtPosition(i);
+                    Log.d("HAR", "onItemSelected: country: "+department.GetDepartmentID());
+                    DepartmentID=department.GetDepartmentID();
+                    Course.clear();
+                    FillCourses(department.GetDepartmentID());
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        Course_Spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if(i>0)
+                {
+                    final Course course = (Course) Course_Spinner.getItemAtPosition(i);
+                    CourseID=course.GetCourseID();
+                    Log.d("HAR", "onItemSelected: course: "+course.GetCourseID());
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+    }
+
+    private void FillCourses(final int DepartmentID)
+    {
+        Course.add(new Course(0, "Course"));
+        final String URL = GlobalMethods.getURL()+"course";
+        progressDialog.show();
+
+        StringRequest jsonObjectRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //JSONArray response = null;
+                Log.d("HAR",response.toString());
+                try {
+                    JSONObject j = new JSONObject(response);
+                    JSONArray data = j.getJSONArray("data");
+                    for(int i=0;i<data.length();i++)
+                    {
+                        JSONObject jsonObject1 = data.getJSONObject(i);
+                        Course.add(new Course(jsonObject1.getInt("Course_id"),
+                                jsonObject1.getString("Course_branch")));
+                    }
+                    Course_Spinner.setAdapter(new ArrayAdapter<Course>(createNotice.this,R.layout.simple_spinner_dropdown_item,Course));
+                    progressDialog.dismiss();
+                }
+                catch (JSONException e){
+
+                }
+                progressDialog.dismiss();
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("HAR",error.toString());
+                progressDialog.dismiss();
+
+
+            }
+        })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parameters = new HashMap<>();
+                Log.d("HAR",String.valueOf(DepartmentID));
+                parameters.put("dept_id", String.valueOf(DepartmentID));
+                return parameters;
+            }
+        };
+        Singleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+    }
+
+    private void FillDepartment() {
+        Department.add(new Department(0, "Department"));
+        final String URL = GlobalMethods.getURL() + "department";
+        progressDialog.show();
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.POST, URL, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        //JSONArray response = null;
+                        Log.d("HAR", response.toString());
+                        try {
+                            JSONArray data = response.getJSONArray("data");
+                            for (int i = 0; i < data.length(); i++) {
+                                JSONObject jsonObject1 = data.getJSONObject(i);
+                                Department.add(new Department(jsonObject1.getInt("Dept_id"),
+                                        jsonObject1.getString("Dept_name")));
+                            }
+                            Department_Spinner.setAdapter(new ArrayAdapter<Department>(createNotice.this, R.layout.simple_spinner_dropdown_item, Department));
+                            progressDialog.dismiss();
+                        } catch (JSONException e) {
+
+                        }
+                        progressDialog.dismiss();
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("HAR", error.toString());
+                        progressDialog.dismiss();
+
+
+                    }
+                });
+
+        Singleton.getInstance(this).addToJsonRequestQueue(jsonObjectRequest);
+    }
+
 
     public void attachment_file_upload(View view){
         Intent chooseFile = new Intent(Intent.ACTION_GET_CONTENT);
